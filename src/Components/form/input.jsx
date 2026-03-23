94f3cfd7a9dbf381d2dfe5f66eb247942b660d7f
runawayfromhome <pengeskinnihayabusa@gmail.com>
2026-03-23 10:45:52 +0800

    push naten to

---
+++ src/Components/form/input.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Input = ({ label, type, placeholder, name }) => {
+    return (
+        <div>
+            <fieldset className="fieldset">
+                <legend className="fieldset-legend">{label}</legend>
+                <input
+                    name={name}
+                    type={type}
+                    className="input"
+                    placeholder={placeholder}
+
+
+                />
+
+
+            </fieldset>
+        </div>
+    );
+
+}
+
+export default Input
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,43 @@
 import MainLayout from "../layouts/MainLayouts";
+import Input from "../Components/form/input";
+import Card from "../Components/Card";
 
 const SignUp = () => {
-	return <MainLayout>This is the Signup</MainLayout>;
+	return (
+		<MainLayout>
+			<div className="flex justify-center">
+				<Card>
+					<h1>Sign up</h1>
+					<Input
+						name="firstname"
+						placeholder="Enter your First name"
+						label="Firstname"
+						type="text"
+					/>
+
+					<Input
+						name="lastname"
+						placeholder="Enter your Last name"
+						label="Lastname"
+						type="text"
+					/>
+					<Input
+						name="email"
+						placeholder="Enter your Email"
+						label="Email"
+						type="email"
+					/>
+					<Input
+						name="password"
+						placeholder="Enter your Password"
+						label="Password"
+						type="password"
+					/>
+					<button className="btn btn-primary">Submit</button>
+				</Card>
+			</div>
+		</MainLayout>
+	);
 };
 
 export default SignUp;
